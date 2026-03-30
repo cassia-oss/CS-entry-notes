5aba3f22774c57e3664e8f2c2aaf8e77bd2f19a4
cassia-oss <2023200199@ruc.edu.cn>
2026-03-30 19:25:21 +0800

    commit

--- 第4周第1次/txt分节计数排序counter.py
+++ 第4周第1次/txt分节计数排序counter.py
@@ -1,14 +1,15 @@
 import jieba
 from collections import defaultdict
 txt = open('D:\Vault\school\junior_last\python 与人工智能\python与人工智能-代码\第3周第2次\高瓴人工智能学院简介.txt', 'r', encoding='utf-8').read()
-positions = defaultdict(list) # 默认值是list函数返回的一个空列表
+positions = defaultdict(list)
 
-for word,start_pots,end_pots in jieba.tokenize(txt):
+for word,start_point,end_point in jieba.tokenize(txt):
     if len(word) > 1:
-           positions[word].append((start_pots,end_pots))
+        positions[word].append((start_point,end_point))
 
-results = sorted(positions.items(),key= lambda x:(-len(x[1]),x[0]))
+results = sorted(positions.items(),key=lambda x:(-len(x[1]),x[0]))
 
-for word,position in results[:15]:
-    print(word)
-    print(positions)
+for word, pos_list in results[:15]:
+    print("{0:<10}{1:>5}".format(word, len(pos_list)))
+
+print(results)
